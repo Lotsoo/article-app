0c6f81b154050d1e36ddf2e75081d5a7fb7c8557
lotsoo <braindeathrama@gmail.com>
2024-05-23 17:52:26 +0700

    Added image

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -12,6 +14,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
@@ -32,3 +36,22 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun ArticleApp(modifier: Modifier = Modifier) {
+    ArticleCard(
+        image = painterResource(R.drawable.bg_compose_background),
+    )
+}
+
+@Composable
+fun ArticleCard(image: Painter, modifier: Modifier = Modifier) {
+    Column {
+        Image(painter = image, contentDescription = null)
+    }
+}
+
+@Preview
+@Composable
+private fun ArticleAppPreview() {
+    ArticleApp()
+}
